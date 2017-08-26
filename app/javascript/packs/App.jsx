5081cef36872269696ee1213d4ffdcd3934dd636
Liz Tiller <elizabeth.tiller@gmail.com>
2017-08-26 15:24:07 -0400

    qr reader jsx/div created

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from './button'
-import qrReader from './qrReader'
+// import qrReader from './qrReader'
 // import styles from './assets/stylesheets/pages'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <div>
       <Button />
-      <qrReader />
+      {/* <qrReader /> */}
     </div>,
     document.getElementById('main')
   )
--- app/javascript/packs/qrReader.js
+++ app/javascript/packs/qrReader.js
@@ -4,37 +4,50 @@ import QrReader from 'react-qr-reader'
 
 // import styles from './assets/stylesheets/pages'
 class qrReader extends Component {
-  constructor(props){
+  constructor (props) {
     super(props)
     this.state = {
       delay: 100,
-      result: 'No result',
+      result: 'No result'
     }
 
     this.handleScan = this.handleScan.bind(this)
+    this.openImageDialog = this.openImageDialog.bind(this)
   }
-  handleScan(data){
-    this.setState({
-      result: data,
-    })
+  handleScan (result) {
+    if (result) {
+      this.setState({ result })
+    }
   }
-  handleError(err){
+
+  // handleScan (data) {
+  //   this.setState({
+  //     result: data
+  //   })
+  // }
+  handleError (err) {
     console.error(err)
   }
-  render(){
+  openImageDialog () {
+    this.refs.qrReader1.openImageDialog()
+  }
+
+  render () {
     const previewStyle = {
       height: 240,
-      width: 320,
+      width: 320
     }
 
-    return(
+    return (
       <div>
         <QrReader
+          ref='qrReader'
           delay={this.state.delay}
           style={previewStyle}
           onError={this.handleError}
           onScan={this.handleScan}
-          />
+          legacyMode />
+        <input type='button' value='Submit QR Code' onClick={this.openImageDialog} />
         <p>{this.state.result}</p>
       </div>
     )
