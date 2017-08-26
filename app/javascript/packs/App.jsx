129101a4399c4eec2ad3ec57441f340ea9d4ceca
Liz Tiller <elizabeth.tiller@gmail.com>
2017-08-26 14:19:58 -0400

    react connection

---
+++ app/javascript/packs/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Button from './button'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Button/>,
+    document.getElementById('main')
+  )
+})
