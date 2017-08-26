acd0adc7e70c2f64ff7766f448083910988b9245
Liz Tiller <elizabeth.tiller@gmail.com>
2017-08-26 14:46:45 -0400

    qrreader

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from './button'
+// import styles from './assets/stylesheets/pages'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Button/>,
+    <div>
+      <Button/>
+    </div>,
     document.getElementById('main')
   )
 })
