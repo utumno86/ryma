8fc6dc1130a1655b95491917d4c5b8155938b564
Liz Tiller <elizabeth.tiller@gmail.com>
2017-08-26 20:38:44 -0400

    took out git bullshit

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -5,17 +5,10 @@ import React, { Component } from 'react'
 class Header extends React.Component {
   render () {
     return (
-<<<<<<< HEAD
-      <div className="trd-tablet-header-base">
-        <div className="trd-body-wrapper">
-          <div className="trd-header-logo">
-          <img src="../assets/V_logo_h_cruises_2x.png" width="320" alt="Vicking logo"/>
-=======
       <div className='trd-tablet-header-base'>
         <div className='trd-body-wrapper'>
           <div className='trd-header-logo'>
             <img src='../assets/V_logo_h_cruises_2x.png' />
->>>>>>> 503fe44f7c925d2631bd216181089949f0dfeb10
           </div>
         </div>
 
