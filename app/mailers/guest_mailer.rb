94838db01375b8c084c7ef06b334ee34f39730f6
Liz Tiller <elizabeth.tiller@gmail.com>
2017-08-27 16:36:41 -0400

    mailer

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -1,12 +1,12 @@
-class GuestMailer < ApplicationMailer
-  include SendGrid
-  sendgrid_category :use_subject_lines
-  sendgrid_enable   :ganalytics, :opentrack
-
-  def welcome_message(guest)
-    email_body = ""
-    mail :to => guest.email, :subject => "Welcome #{guest.first} #{guest.last} :-)"
-  end
-
-
-end
+# class GuestMailer < ApplicationMailer
+#   include SendGrid
+#   sendgrid_category :use_subject_lines
+#   sendgrid_enable   :ganalytics, :opentrack
+#
+#   def welcome_message(guest)
+#     email_body = ""
+#     mail :to => guest.email, :subject => "Welcome #{guest.first} #{guest.last} :-)"
+#   end
+#
+#
+# end
--- app/mailers/guest_notifier_mailer.rb
+++ app/mailers/guest_notifier_mailer.rb
@@ -1,10 +1,10 @@
-class GuestNotifierMailer < ApplicationMailer::Base
-  default :from => 'any_from_address@example.com'
-
-  # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_guest_notification(guest)
-    @guest = guest
-    mail( :to => @guest.email,
-    :subject => 'Your bag has arrived' )
-  end
-end
+# class GuestNotifierMailer < ApplicationMailer::Base
+#   default :from => 'any_from_address@example.com'
+#
+#   # send a signup email to the user, pass in the user object that   contains the user's email address
+#   def send_guest_notification(guest)
+#     @guest = guest
+#     mail( :to => @guest.email,
+#     :subject => 'Your bag has arrived' )
+#   end
+# end
