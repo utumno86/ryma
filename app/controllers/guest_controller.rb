a458ab43b9512be44d0748f67642cbd8ff370186
mnash <utumno86@gmail.com>
2017-08-27 12:30:30 -0400

    convert to API work in progress

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -1,2 +1,6 @@
-class GuestController < ApplicationController
+class GuestController < Api:BaseController
+  def update
+    @guest = Guest.find_by reservation: params[:reservation]
+    @guest.baggages.create(guest_id: @guest.id, qrcode: @guest.reservation, delivered: false)
+  end
 end
