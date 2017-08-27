e95997546fafbb834b6e2ae0ccc3b6d3d48ff870
Liz Tiller <elizabeth.tiller@gmail.com>
2017-08-27 14:34:28 -0400

    cleaned up some things

--- app/javascript/packs/guestStatus.js
+++ app/javascript/packs/guestStatus.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'  
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import footerIcons from './images/footer-icons.png'
 
 class GuestStatus extends React.Component {
   constructor(props) {
@@ -13,13 +14,13 @@ class GuestStatus extends React.Component {
 
     return (
       <div className='trd-tablet-header-base'>
-         
+
        <p>Welcome aboard the Viking Ra,
            <br/>
              Your luggage is being taken care of by our capable bell staff.  Keep this page open to track the status of your bags.</p>
 
         <div className='center'>
-            <img src="../assets/footer-icons.png" width="320px"/>
+            <img src={footerIcons} width="320px"/>
         </div>
       </div>
 
--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'  
+import React, { Component } from 'react'
+import V_logo_h_cruises_2x from'./images/V_logo_h_cruises_2x.png'
 // import 'bootstrap/dist/css/bootstrap.css'
 // import { Col } from 'react-bootstrap'
 
 class Header extends React.Component {
-  
+
 
   render () {
     //var image_path = require()
@@ -11,7 +12,7 @@ class Header extends React.Component {
       <div className='trd-tablet-header-base'>
         <div className='header-base'>
           <div className='trd-header-logo'>
-            <img src='../assets/V_logo_h_cruises_2x.png' width="236" />
+            <img src={V_logo_h_cruises_2x} width="236" />
           </div>
         </div>
 
