34a3c2b08eaa5b571a0a19b257152d0cb0d32dbb
mnash <utumno86@gmail.com>
2017-09-24 20:30:44 -0400

    added update function

--- app/controllers/api/guests_controller.rb
+++ app/controllers/api/guests_controller.rb
@@ -2,8 +2,13 @@ class Api::GuestsController < Api::BaseController
   def index
   end
   def show
-    puts params
     @guest = Guest.find_by reservation: params[:id]
+    respond_with :api, json: @guest
+  end
+  def update
+    @guest = Guest.find_by reservation: params[:id]
+    @guest.bagstatus = true
+    @guest.save
     respond_with :api, json: @guest
   end
 end
